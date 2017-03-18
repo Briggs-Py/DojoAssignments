59d7cfc34c8461e9caaf5674830f9ec244108727
Briggs McKnight <briggs.mcknight@gmail.com>
2017-03-17 18:33:14 -0700

    Adding Django files and updating earlier files.

--- Python/flaskAndMySQL/loginAndRegistration/server.py
+++ Python/flaskAndMySQL/loginAndRegistration/server.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, redirect, request, session, flash
 from mysqlconnection import MySQLConnector
-from datetime import datetime
+from flask.ext.bcrypt import Bcrypt
 import re
 
 app = Flask(__name__)
-mysql = MySQLConnector(app,'cdTheWall')
+mysql = MySQLConnector(app,'emailRegister')
+bcrypt = Bcrypt(app)
 app.secret_key = "ThisIsSecret!"
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
@@ -13,21 +14,25 @@ EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 def index():
     return render_template('index.html')
 
-@app.route('/register')
+@app.route('/register', methods=['POST'])
 def register():
     valid = True
 
     for field in request.form:
         if len(request.form[field]) == 0:
-            flash(field + " mut not be empty")
+            flash(field + " must not be empty")
             valid = False
 
     if len(request.form['first_name']) < 2 or len(request.form['last_name']) < 2:
         flash("name must be more that two characters")
         valid = False
 
+    if not EMAIL_REGEX.match(request.form['email']):
+        flash("Please Enter A Valid Email")
+        valid = False
+
     if not request.form['password'] == request.form['confirm']:
-        flash("Password must match Confirm Password")
+        flash("Password Must Match Confirm Password")
         valid = False
 
     if len(request.form['password']) < 8:
@@ -37,14 +42,38 @@ def register():
     if(valid == False):
         return redirect('/')
 
+    password = request.form['password']
+    pw_hash = bcrypt.generate_password_hash(password)
+
     input_data = {
         'fname': request.form['first_name'],
         'lname': request.form['last_name'],
         'email': request.form['email'],
-        'password': request.form['password']
+        'pw_hash': pw_hash
     }
 
-    user_data = (INSERT INTO users (first_name, last_name, email, password))
+    user_data = "INSERT INTO users (first_name, last_name, email, password) VALUES (:fname, :lname, :email, :pw_hash)"
+
+    add_user = mysql.query_db(user_data, input_data)
+    flash("Success!")
+    return redirect('/')
+
+@app.route('/login', methods=['POST'])
+def login():
+    data =  {'email': request.form['email']}
+    user_email = mysql.query_db("SELECT first_name, email, password FROM users WHERE email = :email", data)
+    print user_email
+    if user_email:
+        if bcrypt.check_password_hash(user_email[0]['password'], request.form['password']):
+            session['user'] = user_email[0]['first_name']
+            flash("Welcome,  "+ str(session['user']))
+        else:
+            flash('Incorrect Password')
+    else:
+        flash("Invalid Email")
+
+
+    return redirect('/')
 
 
 
--- Python/flaskAndMySQL/theWall/server.py
+++ Python/flaskAndMySQL/theWall/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, request, session, flash
 from mysqlconnection import MySQLConnector
 from datetime import datetime
-import re
+import bcrypt, re
 
 app = Flask(__name__)
 mysql = MySQLConnector(app,'cdTheWall')
@@ -10,85 +10,112 @@ EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/process', methods=['POST'])
-def process():
-    session['loginEmail'] = request.form['email']
-    session['loginPassword'] = request.form['password']
-    session['dbEmails'] = mysql.query_db("SELECT email, password FROM users")
+@app.route('/createAccount', methods=['POST', 'GET'])
+def createaccount():
+    return render_template('createAccount.html')
 
+@app.route('/register', methods=['POST'])
+def register():
+    valid = True
 
+    for field in request.form:
+        if len(request.form[field]) == 0:
+            flash(field + " must not be empty")
+            valid = False
+
+    if len(request.form['first']) < 2 or len(request.form['last']) < 2:
+        flash("name must be more that two characters")
+        valid = False
+
+    if not EMAIL_REGEX.match(request.form['email']):
+        flash("Please Enter A Valid Email")
+        valid = False
+
+    if not request.form['password'] == request.form['confirm']:
+        flash("Password Must Match Confirm Password")
+        valid = False
+
+    if len(request.form['password']) < 8:
+        flash("Password must be at least 8 characters")
+        valid = False
+
+    if(valid == False):
+        return redirect('/createAccount')
+
+    input_data = {
+        'fname': request.form['first'],
+        'lname': request.form['last'],
+        'email': request.form['email'],
+        'password': bcrypt.hashpw(request.form['password'].encode('utf8'), bcrypt.gensalt())
+    }
+
+    user_data = "INSERT INTO users (first_name, last_name, email, password, created_at, updated_at) VALUES (:fname, :lname, :email, :password, NOW(), NOW())"
+
+    add_user = mysql.query_db(user_data, input_data)
+    flash("Success!")
+
+    session['user'] = add_user
+    session['name'] = request.form['first']
     return redirect('/wall')
 
-
-@app.route('/wall', methods=['POST', "GET"])
+@app.route('/login', methods=['POST'])
 def login():
-    return render_template("wall.html")
+    data =  {'email': request.form['email']}
+    user_email = mysql.query_db("SELECT id, first_name, email, password FROM users WHERE email = :email", data)
+    print user_email
+    if user_email:
+        if bcrypt.hashpw(request.form['password'].encode('utf8'), user_email[0]['password'].encode('utf8')) == user_email[0]['password']:
+            session['user'] = user_email[0]['id']
+            session['name'] = user_email[0]['first_name']
+            return redirect('/wall')
+        else:
+            flash('Incorrect Password')
+    else:
+        flash("Invalid Email")
 
-@app.route('/createaccount', methods=['POST', "GET"])
-def create():
-    return render_template("createAccount.html")
 
-@app.route('/newaccount', methods=['POST'])
-def newAccount():
+    return redirect('/')
 
-#Validation
-    if len(request.form['email']) < 1:
-        flash('Email Cannot Be Blank', 'error')
-        return redirect('/')
-    elif not EMAIL_REGEX.match(request.form['email']):
-        flash('Email Must Be Valid', 'error')
-        return redirect('/')
-    if len(request.form['first']) < 1:
-        flash('First Name Cannot Be Blank', 'error')
-        return redirect('/')
-    elif not (request.form['first']).isalpha():
-        flash('First Name Cannot Contain Numbers', 'error')
-        return redirect('/')
-    if len(request.form['last']) < 1:
-        flash('Last Name Cannot Be Blank', 'error')
-        return redirect('/')
-    elif not request.form['last'].isalpha():
-        flash('Last Name Cannot Contain Numbers', 'error')
-        return redirect('/')
-    if len(request.form['password']) < 1:
-        flash('Password Cannot Be Blank', 'error')
-        return redirect('/')
-    elif len(request.form['password']) < 8:
-        flash('Password Must Be At Least 8 Characters', 'error')
-        return redirect('/')
-    if len(request.form['confirm']) < 1:
-        flash('You Must Confirm Your Password', 'error')
-        return redirect('/')
-    elif request.form['confirm'] != request.form['password']:
-        flash('Password Fields Must Match', 'error')
-        return redirect('/')
-    
+@app.route('/wall')
+def wall():
+	messages = mysql.query_db("SELECT CONCAT(users.first_name, ' ', users.last_name) AS name, posts.*  FROM posts JOIN users ON users.id = posts.users_id ORDER BY created_at DESC;")
+	for message in messages:
+		data = {
+			'message_id': message['id']
+		}
+		message['comments'] = mysql.query_db("SELECT comments.*, CONCAT(users.first_name, ' ', users.last_name) AS name FROM comments JOIN users ON users.id = comments.users_id WHERE posts_id = :message_id", data)
 
+	return render_template('wall.html', messages=messages)
+
+@app.route('/post', methods=['POST'] )
+def post():
+    data = {
+		'user_id': session['user'],
+		'message': request.form['post']
+	}
+    message_insert_query = "INSERT INTO posts (users_id, post) VALUES(:user_id, :message);"
+    message_id = mysql.query_db(message_insert_query, data)
     return redirect('/wall')
 
-@app.route('/post', methods=['POST', 'GET'] )
-def post():
-    if len(request.form['post']) < 1:
-        flash("Post Cannot Be Blank!")
-        return redirect('/wall')
-    info ={'post':request.form['post'],
-    'created': datetime.today()}
-    my_post = mysql.query_db("INSERT INTO users (post, created_at) VALUES(:post, :created);", info)
-    session['posts']=""
-    session['post']=mysql.query_db("SELECT post, created_at FROM posts")
-    for data in session['post']:
-      date = data['created_at'].strftime('%c')
-      session['posts']+='<p>'+data['post']+' '+ date +'</p>'
-    return render_template('submit.html')
-
-@app.route('/list')
+@app.route('/comment')
 def list():
-  session['posts']=""
-  session['post']=mysql.query_db("SELECT post, created_at FROM posts")
-  for data in session['post']:
-    date = data['created_at'].strftime('%c')
-    session['posts']+='<p>'+data['post']+' '+ date +'</p>'
-  return render_template('submit.html')
+    data = {
+		'user_id': session['user'],
+		'post_id': request.form['post_id'],
+		'comment': request.form['comment']
+	}
+    comment_insert_query = "INSERT INTO comments (users_id, posts_id, comment) VALUES(:user_id, :post_id, :comment);"
+    mysql.query_db(comment_insert_query, data)
+    return redirect('/wall')
+
+@app.route('/logout')
+def logout():
+	if 'user' in session:
+		session.pop('user')
+	if 'name' in session:
+		session.pop('name')
+	return redirect('/')
+
 app.run(debug=True)
